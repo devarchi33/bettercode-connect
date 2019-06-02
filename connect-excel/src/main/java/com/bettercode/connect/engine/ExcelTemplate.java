b9a79198bffe9f1e4a28c5f395c3134782e0dc9e
devarchi33 <devarchi33@gmail.com>
2019-06-02 13:53:44 +0800

    #11 Refactoring SheetContentHandler

--- connect-excel/src/main/java/com/bettercode/connect/engine/ExcelTemplate.java
+++ connect-excel/src/main/java/com/bettercode/connect/engine/ExcelTemplate.java
@@ -1,5 +1,6 @@
 package com.bettercode.connect.engine;
 
+import com.bettercode.connect.engine.handler.SheetContentHandlerBuilder;
 import com.bettercode.connect.entity.ExcelFile;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.exceptions.NotOfficeXmlFileException;
@@ -7,13 +8,11 @@ import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.openxml4j.opc.PackageAccess;
 import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.util.SAXHelper;
 import org.apache.poi.xssf.eventusermodel.ReadOnlySharedStringsTable;
 import org.apache.poi.xssf.eventusermodel.XSSFReader;
 import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler;
 import org.apache.poi.xssf.model.StylesTable;
-import org.apache.poi.xssf.usermodel.XSSFComment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.ContentHandler;
@@ -31,66 +30,23 @@ public class ExcelTemplate {
     private final static Logger logger = LoggerFactory.getLogger(ExcelTemplate.class);
 
     public List<Object> getRows(ExcelFile uploadExcelFile, ExcelRowMapper excelRowMapper) throws IOException {
-        List<Object> objectList = new ArrayList<>();
+        SheetContentHandlerBuilder sheetContentHandlerBuilder = new SheetContentHandlerBuilder(new ArrayList<>());
+
         try {
             try (OPCPackage opcPackage = OPCPackage.open(uploadExcelFile.getOriginalFile().getPath(), PackageAccess.READ)) {
                 ReadOnlySharedStringsTable strings = new ReadOnlySharedStringsTable(opcPackage);
                 XSSFReader xssfReader = new XSSFReader(opcPackage);
                 StylesTable styles = xssfReader.getStylesTable();
                 XSSFReader.SheetIterator iter = (XSSFReader.SheetIterator) xssfReader.getSheetsData();
-
                 try (InputStream stream = iter.next()) {
                     DataFormatter formatter = new DataFormatter();
                     InputSource sheetSource = new InputSource(stream);
                     try {
                         XMLReader sheetParser = SAXHelper.newXMLReader();
                         ContentHandler handler = new XSSFSheetXMLHandler(
-                            styles, null, strings, new XSSFSheetXMLHandler.SheetContentsHandler() {
-                            private int currentRowIndex = -1;
-                            private int currentColumnIndex = -1;
-                            private boolean firstRow = true;
-
-                            private Object object = null;
-
-                            @Override
-                            public void startRow(int rowNum) {
-                                if(rowNum == 0) {
-                                    firstRow = true;
-                                } else {
-                                    firstRow = false;
-                                    object = excelRowMapper.create();
-                                }
-                                currentRowIndex = rowNum;
-                                currentColumnIndex = -1;
-                            }
-
-                            @Override
-                            public void endRow(int rowNum) {
-                                if(this.object != null) {
-                                    objectList.add(object);
-                                }
-                            }
-
-                            @Override
-                            public void cell(String cellReference, String formattedValue, XSSFComment xssfComment) {
-                                if(!firstRow) {
-                                    // Did we miss any cells?
-                                    int thisColumnIndex = (new CellReference(cellReference)).getCol();
-                                    int missedCols = thisColumnIndex - currentColumnIndex - 1;
-                                    for (int i = 1; i <= missedCols; i++) {
-                                        excelRowMapper.mapRow(this.object, currentRowIndex, (currentColumnIndex + i), null);
-                                    }
-
-                                    currentColumnIndex = thisColumnIndex;
-                                    excelRowMapper.mapRow(this.object, currentRowIndex, currentColumnIndex, formattedValue);
-                                }
-                            }
-
-                            @Override
-                            public void headerFooter(String s, boolean b, String s1) {
-                            }
-                        }, formatter, false);
-
+                            styles, null, strings,
+                            sheetContentHandlerBuilder.create(excelRowMapper),
+                            formatter, false);
                         sheetParser.setContentHandler(handler);
                         sheetParser.parse(sheetSource);
                     } catch(ParserConfigurationException e) {
@@ -115,6 +71,6 @@ public class ExcelTemplate {
             }
         }
 
-        return objectList;
+        return sheetContentHandlerBuilder.getExcelDataList();
     }
 }
---
+++ connect-excel/src/main/java/com/bettercode/connect/engine/handler/SheetContentHandlerBuilder.java
@@ -0,0 +1,69 @@
+package com.bettercode.connect.engine.handler;
+
+import com.bettercode.connect.engine.ExcelRowMapper;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler;
+import org.apache.poi.xssf.usermodel.XSSFComment;
+
+import java.util.List;
+
+public class SheetContentHandlerBuilder {
+
+  private List<Object> excelDataList;
+
+  public SheetContentHandlerBuilder(List<Object> excelDataList) {
+    this.excelDataList = excelDataList;
+  }
+
+  public List<Object> getExcelDataList() {
+    return excelDataList;
+  }
+
+  public XSSFSheetXMLHandler.SheetContentsHandler create(ExcelRowMapper excelRowMapper) {
+    return new XSSFSheetXMLHandler.SheetContentsHandler() {
+      private int currentRowIndex = -1;
+      private int currentColumnIndex = -1;
+      private boolean firstRow = true;
+
+      private Object object = null;
+
+      @Override
+      public void startRow(int rowNum) {
+        if(rowNum == 0) {
+          firstRow = true;
+        } else {
+          firstRow = false;
+          object = excelRowMapper.create();
+        }
+        currentRowIndex = rowNum;
+        currentColumnIndex = -1;
+      }
+
+      @Override
+      public void endRow(int rowNum) {
+        if(this.object != null) {
+          excelDataList.add(object);
+        }
+      }
+
+      @Override
+      public void cell(String cellReference, String formattedValue, XSSFComment xssfComment) {
+        if(!firstRow) {
+          // Did we miss any cells?
+          int thisColumnIndex = (new CellReference(cellReference)).getCol();
+          int missedCols = thisColumnIndex - currentColumnIndex - 1;
+          for (int i = 1; i <= missedCols; i++) {
+            excelRowMapper.mapRow(this.object, currentRowIndex, (currentColumnIndex + i), null);
+          }
+
+          currentColumnIndex = thisColumnIndex;
+          excelRowMapper.mapRow(this.object, currentRowIndex, currentColumnIndex, formattedValue);
+        }
+      }
+
+      @Override
+      public void headerFooter(String s, boolean b, String s1) {
+      }
+    };
+  }
+}
